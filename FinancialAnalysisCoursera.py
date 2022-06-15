df631495016bba98de8165e3cb34ece61d5ca03a
mttwdevelops <mttwhwang.works@gmail.com>
2022-06-15 12:29:22 -0700

    financial practice

---
+++ FinancialAnalysisCoursera.py
@@ -0,0 +1,14 @@
+# Course on Python and Statistics for Financial Analysis 
+# Source of course is found here: https://www.coursera.org/learn/python-statistics-financial-analysis
+
+import pandas as pd
+
+fb = pd.read_csv('facebook.csv', index_col=0)
+ms = pd.read_csv('microsoft.csv', index_col=0)
+
+print(fb.head())
+
+# we can slice dataframe by either label (.loc) or by position (.iloc)
+print(fb.loc['2022-01-01':'2022-12-31', 'Close'])
+
+fb.loc['2022-01-01':'2022-12-31', 'Close'].plot()
