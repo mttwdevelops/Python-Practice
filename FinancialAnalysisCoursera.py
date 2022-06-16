d305c9e2a5f9df21874bf769253eae3d4e3d7ab1
mttwdevelops <mttwhwang.works@gmail.com>
2022-06-16 14:31:27 -0700

    updated readme and testing financial analysis on main testing environment

--- FinancialAnalysisCoursera.py
+++ FinancialAnalysisCoursera.py
@@ -2,6 +2,8 @@
 # Source of course is found here: https://www.coursera.org/learn/python-statistics-financial-analysis
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 fb = pd.read_csv('facebook.csv', index_col=0)
 ms = pd.read_csv('microsoft.csv', index_col=0)
@@ -11,4 +13,6 @@ print(fb.head())
 # we can slice dataframe by either label (.loc) or by position (.iloc)
 print(fb.loc['2022-01-01':'2022-12-31', 'Close'])
 
-fb.loc['2022-01-01':'2022-12-31', 'Close'].plot()
+x = fb.loc['2022-01-01':'2022-12-31', 'Close']
+print("hello world")
+plt.plot(x)
