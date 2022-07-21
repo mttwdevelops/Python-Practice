560bd1b10d118883ddda7447be48f73536b011d1
mttwdevelops <mttwhwang.works@gmail.com>
2022-07-20 17:40:32 -0700

    finished scrape tutorial

--- Scrape.py
+++ Scrape.py
@@ -2,17 +2,27 @@
 
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(res.text, 'html.parser')
 # print(res)  # if value is 200, then everything is fine.
 # print(res.text) # gives the html of the website
-soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup.select('.score'))
 
 links = soup.select('.titlelink') # gets the newstitles since the titles are under the 'titlelink' class
 subtext = soup.select('.subtext') # gets the number of upvotes. 
-# print(soup)
-# Since the two above provide a list of all the different entries, we can use [0] to get the first index.
+
+# to grab more than just the first 30 posts:
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+links2 = soup2.select('.titlelink') # gets the newstitles since the titles are under the 'titlelink' class
+subtext2 = soup2.select('.subtext')
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
 
 def create_custom_hn(links, subtext):
     hn = []
@@ -22,10 +32,11 @@ def create_custom_hn(links, subtext):
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
-            print(points)
-        hn.append({'title': title, 'link': href})
-    return hn
+            # print(points)
+            if points >= 100:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
 # print(create_custom_hn(links, subtext))
-# print(links)
-create_custom_hn(links, subtext)
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
+# create_custom_hn(links, subtext)
